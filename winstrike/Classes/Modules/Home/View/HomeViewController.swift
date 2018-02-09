eb883f8994de96453a78d9df29b2f314a933d908
mkdk <eugen_k69@yahoo.com>
2018-02-09 16:01:57 +0300

    fabric integration success & change icon on homescrean

--- winstrike/Classes/Modules/Home/View/HomeViewController.swift
+++ winstrike/Classes/Modules/Home/View/HomeViewController.swift
@@ -60,7 +60,7 @@ class HomeViewController: ParentViewController {
 
     var listButton: UIButton! = {
         let b = UIButton()
-        b.setImage(Asset.HomeScreen.moneyHand.image, for: .normal)
+        b.setImage(Asset.HomeScreen.fileIcon.image, for: .normal)
         return b
     }()
 
